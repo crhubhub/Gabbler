9ecd5e887c8993f1292e7f853160e26b1f4fbd2c
crhubhub <clo73877@gmail.com>
2020-10-11 14:02:16 +0200

    commenting my code

--- controller/public/cl.lost-pass.controller.php
+++ controller/public/cl.lost-pass.controller.php
@@ -1,29 +1,37 @@
 <?php
-// model
+// Loading the necessary bounded model
 include THE_ROOT . DIRECTORY_SEPARATOR . 'model' . DIRECTORY_SEPARATOR . 'cl.lost-pass.model.php';
 
+//Checking if the email to check was setted
 if (isset($_POST['email'])) {
     $claimedEmail = $_POST['email'];
-//if (true) {
-//    $email = $_POST['email'];
+
+//    Checking that the setted email is a 'valid email format' input
     if (filter_var($claimedEmail, FILTER_VALIDATE_EMAIL)) {
-//    if (true) {
-        $req = "SELECT * FROM user";
+
+//        Sql request
+        $req = "SELECT nickname_user, mail_user FROM user";
+//        Req exec
         $result = mysqli_query($db, $req);
+//        Just checking if the request was correct comparing to the db...
         $requestIsValid = mysqli_num_rows($result);
-        var_dump($requestIsValid);
         if ($requestIsValid) {
+//            Parsing and variables creation
             while ($row = mysqli_fetch_assoc($result)) {
                 $distantEmail = ($row['mail_user']);
+                $distantUser = ($row['nickname_user']);
             }
             if ((isset($distantEmail) && ($distantEmail === $claimedEmail))) {
-                $infoMessage = 'same email';
+//                Sending an email which helps the user to reconnect
+//                ...and display a message which leaves the whole mystery about the 
+                $cl_infoMessageDEBUG = 'existing email';
             } else {
-                $infoMessage = 'different email';
+                $cl_infoMessageDEBUG = 'non-existing email';
             }
         } else {
-            $infoMessage = 'non existing claimed email';
+            $cl_infoMessageDEBUG = 'Request problem, maybe check table(s) name(s)';
         }
+
 
     }
 }
--- model/cl.lost-pass.model.php
+++ model/cl.lost-pass.model.php
@@ -1,2 +1,3 @@
 <?php
 require_once dirname(__DIR__) . DIRECTORY_SEPARATOR . 'model' . DIRECTORY_SEPARATOR . 'connectToDB.model.php';
+
